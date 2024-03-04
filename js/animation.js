565987f08d7e3c82b730be5128ae2ca3cb4027b0
Hnng2011 <luphihung111@gmail.com>
2024-03-04 15:07:35 +0700

    update anmation apper text

--- js/animation.js
+++ js/animation.js
@@ -4,12 +4,10 @@ window.addEventListener('load', (event) => {
 });
 
 function updateImageSources(count) {
-    console.log('count', count);
     const maskedImages = document.querySelectorAll('.masked_imgage');
 
     maskedImages.forEach((masked, index) => {
         const newSrc = index === 0 ? `./assets/images/NFT/${count}.png` : `./assets/images/NFT/${89 - count}.png`;
-        console.log(`Setting src for maskedImages[${index}] to ${newSrc}`);
         masked.src = newSrc;
     });
 
@@ -56,29 +54,67 @@ function getRandomValue(min, max) {
 function createRandomText() {
     const textArray = ['Buy LOS!!!', 'To Da Moonnnn !!!!', 'Welcome to LOS!!', 'Mintingggg'];
     const animationContainer = document.getElementById('animationContainer');
+    const animationContainer2 = document.getElementById('animationContainer_2');
+    const logoHeader = document.querySelector('.header_logo');
+    const buttonHeader = document.querySelector('.header_function button');
+    const logoHeaderRect = logoHeader.getBoundingClientRect();
+    const buttonHeaderRect = buttonHeader.getBoundingClientRect();
 
-    const numberOfTexts = getRandomValue(1, 3);
-
-    for (let i = 0; i < numberOfTexts; i++) {
+    for (let i = 0; i < 4; i++) {
+        const existingTexts = document.querySelectorAll('.animated_text');
+        console.log(existingTexts);
         const randomText = document.createElement('div');
-        randomText.classList.add('animated-text');
+        randomText.classList.add('animated_text');
         randomText.textContent = textArray[getRandomValue(0, textArray.length - 1)];
 
-        const randomTop = getRandomValue(0, window.innerHeight);
-        const randomLeft = getRandomValue(0, window.innerWidth);
-        const randomRotation = getRandomValue(0, 45);
+        let isOverlapping = false;
+        do {
+            isOverlapping = false;
+            const randomTop = getRandomValue(0, window.innerHeight);
+            const randomLeft = getRandomValue(0, window.innerWidth);
+            const randomRotation = getRandomValue(0, 45);
 
-        randomText.style.top = `${randomTop}px`;
-        randomText.style.left = `${randomLeft}px`;
-        randomText.style.rotate = `${getRandomValue(0, 1) === 0 ? `-${randomRotation}` : `+${randomRotation}`}deg`;
+            randomText.style.top = `${randomTop}px`;
+            randomText.style.left = `${randomLeft}px`;
+            randomText.style.rotate = `${getRandomValue(0, 1) === 0 ? `-${randomRotation}` : `+${randomRotation}`}deg`;
+            animationContainer2.appendChild(randomText);
 
+            const randomRect = randomText.getBoundingClientRect();
+
+
+            if (
+                randomRect.top <= buttonHeaderRect.bottom &&
+                randomRect.bottom >= logoHeaderRect.top &&
+                randomRect.left <= logoHeaderRect.right &&
+                randomRect.right >= logoHeaderRect.left
+            ) {
+                isOverlapping = true;
+            }
+
+            existingTexts.forEach(existingText => {
+                const existingRect = existingText.getBoundingClientRect();
+
+                if (
+                    randomRect.top <= existingRect.bottom &&
+                    randomRect.bottom >= existingRect.top &&
+                    randomRect.left <= existingRect.right &&
+                    randomRect.right >= existingRect.left
+                ) {
+                    isOverlapping = true;
+                }
+            });
+
+        } while (isOverlapping);
+
+        animationContainer2.removeChild(randomText)
         animationContainer.appendChild(randomText);
-
         randomText.addEventListener('animationend', () => {
             animationContainer.removeChild(randomText);
         });
     }
 }
+
+
 
 
 
